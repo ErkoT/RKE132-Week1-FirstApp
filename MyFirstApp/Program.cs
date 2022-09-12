36c49f55b74e127cd5737411e529d7e6ee6a3135
Erko Targamaa <erkotargamaa@gmail.com>
2022-09-12 18:51:22 +0300

    app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,16 @@
+﻿//Kommentaar
+
+//Nime küsimine
+//Tervistus
+
+Console.WriteLine("Sisesta oma nimi: "); //Väljund
+
+//string on sõne
+
+string userName = Console.ReadLine(); //Sisend
+
+Console.WriteLine("Hello, " + userName + "!"); //Väljund
+
+//string interpolation
+
+Console.WriteLine($"Hello, {userName}!"); //Väljund
